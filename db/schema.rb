5c09afcc1feb92ba4366fc6061efc1f3148dad6e
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-16 15:50:37 +0700

    Create artis.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_084840) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_085002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artis", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blocked_ip_addresses", force: :cascade do |t|
     t.string "ip_address", null: false
