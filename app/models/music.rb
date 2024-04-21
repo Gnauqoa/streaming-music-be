22215e2ef58d022c24595d4cfdc085887efa7898
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-21 21:05:48 +0700

    Update music model.

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,9 @@
 class Music < ApplicationRecord
   belongs_to :artist
+  has_many :playlist_musics
+  has_many :user_likes
+
+  def liked_by_user?(user_id)
+    user_likes.where(user_id:).exists?
+  end
 end
