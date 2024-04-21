f4b2ee1a3bd881c04270c12d14bd22a94a933872
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-21 22:11:55 +0700

    Add user like music video.

--- app/api/users/musics.rb
+++ app/api/users/musics.rb
@@ -9,7 +9,23 @@ module Users
         music = Music.find(params[:id])
         return error!([:music_not_found], 401) if music.nil?
 
-        format_response(music)
+        format_response(music, serializer: MusicSerializer, scope: { current_user: })
+      end
+
+      desc 'Like music'
+      params do
+        requires :id, type: Integer, desc: 'Music ID'
+      end
+      post ':id/like' do
+        music = Music.find(params[:id])
+        return error!([:music_not_found], 401) if music.nil?
+        return error!([:music_already_liked], 401) if music.liked_by_user?(current_user.id)
+
+        current_user.user_likes.create!(music:)
+        music.update(
+          likes_count: music.likes_count + 1
+        )
+        format_response(music, serializer: MusicSerializer, scope: { current_user: })
       end
     end
   end
