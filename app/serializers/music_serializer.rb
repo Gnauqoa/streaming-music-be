ac9e0daad6750fab331e249a9f983e0589fc74f7
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-21 22:11:48 +0700

    Add liked to music_serializer.

--- app/serializers/music_serializer.rb
+++ app/serializers/music_serializer.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class MusicSerializer < ActiveModel::Serializer
-  attributes :id, :name, :likes_count, :artist, :source_url, :thumbnail_url, :created_at, :updated_at
+  attributes :id, :name, :artist, :like_count, :liked, :source_url, :thumbnail_url, :created_at, :updated_at
+
+  def liked
+    return nil if scope.nil? || scope[:current_user].nil?
+
+    object.liked_by_user?(scope[:current_user].id)
+  end
 end
