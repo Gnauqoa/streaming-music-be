7449f86a67348f13bf88ac1830385d7a22f4ccfa
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-23 14:46:09 +0700

    Add artist update and delete music API.

--- app/api/artists/musics.rb
+++ app/api/artists/musics.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Artists
-  class Musics < PublicBase
+  class Musics < Base
     resources :musics do
       desc 'Create Music',
            summary: 'Create Music'
@@ -12,7 +12,37 @@ module Artists
         requires :source_url, type: String, desc: 'Music source url'
       end
       post do
-        music = Music.create!(declared(params))
+        music = Music.create!(declared(params).merge(artist_id: current_artist.id))
+        format_response(music)
+      end
+
+      desc 'Update Music',
+           summary: 'Update Music'
+      params do
+        requires :id, type: Integer, desc: 'Music id'
+        optional :name, type: String, desc: 'Music name'
+        optional :description, type: String, desc: 'Music description'
+        optional :thumbnail_url, type: String, desc: 'Music thumbnail url'
+        optional :source_url, type: String, desc: 'Music source url'
+      end
+      put do
+        return error!('Music not found', 404) unless ArtistPolicy.new(current_artist:).manage_music(params[:id])
+
+        music = Music.find(params[:id])
+        music.update!(declared(params, include_missing: false))
+        format_response(music)
+      end
+
+      desc 'Delete Music',
+           summary: 'Delete Music'
+      params do
+        requires :id, type: Integer, desc: 'Music id'
+      end
+      delete do
+        return error!('Music not found', 404) unless ArtistPolicy.new(current_artist:).manage_music(params[:id])
+
+        music = Music.find(params[:id])
+        music.destroy
         format_response(music)
       end
     end
