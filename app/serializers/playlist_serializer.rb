d1dedd5cb8e9e97f2adb0800adc21e11b5fb07b2
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-04-24 10:18:49 +0700

    Add liked and likes_count to model and serializer of playlist.

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :musics, :user, :created_at, :updated_at
+  attributes :id, :name, :musics, :user, :liked, :likes_count, :created_at, :updated_at
+
+  def liked
+    return nil if scope.nil? || scope[:current_user].nil?
+
+    object.liked_by_user?(scope[:current_user].id)
+  end
 end
