5ac4beeacc1b664bb8c26fdc0c7792dce52baa12
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-18 14:54:51 +0700

    Remove ip field in user profile serializer.

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserProfileSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :first_name, :last_name, :created_at, :updated_at,:current_sign_in_at, :current_sign_in_ip
+  attributes :id, :username, :email, :first_name, :last_name, :created_at, :updated_at
 end
