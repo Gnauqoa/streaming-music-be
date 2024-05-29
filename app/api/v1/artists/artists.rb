4a53ee152d9d4104d25c4c3f50f4e5424305a68b
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-29 20:01:02 +0700

    Add create artist api.

--- app/api/v1/artists/artists.rb
+++ app/api/v1/artists/artists.rb
@@ -8,6 +8,35 @@ module V1
 
           log_ip(artist.id, client_ip)
         end
+        desc "Sign Up",
+            summary: 'Sign Up'
+        params do
+          requires :email, type: String,
+                          desc: 'Email'
+          requires :password, type: String, desc: 'The login password'
+          requires :password_confirmation, type: String, desc: 'The login password confirmation'
+          requires :birth, type: Date, desc: 'The birth date'
+          requires :name, type: String, desc: 'The artist name'
+        end
+        post do
+          return error!('Password confirmation does not match', 422) if params[:password] != params[:password_confirmation]
+          return error!('Email already exists', 422) if Artist.find_by_email(params[:email])
+          artist = Artist.create!(
+            email: params[:email],
+            password: params[:password],
+            birth: params[:birth],
+            name: params[:name]
+          )
+
+          if artist
+            status 201
+            generate_token_result = GenerateToken.new(artist:).call
+            token = generate_token_result.success
+            format_response({ access_token: token })
+          else
+            error!(failure_response(*@result.failure), 422)
+          end
+        end
 
         desc 'Sign In',
             summary: 'Sign In'
