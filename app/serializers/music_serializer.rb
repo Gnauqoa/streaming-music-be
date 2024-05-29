c2c983ccf8d6e4afe227e2ed1b5162ca30798ff2
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-29 22:38:12 +0700

    Add image url to music.

--- app/serializers/music_serializer.rb
+++ app/serializers/music_serializer.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class MusicSerializer < ActiveModel::Serializer
-  attributes :id, :name, :likes_count, :images, :liked, :source_url, :created_at, :updated_at
+  attributes :id, :name, :likes_count, :image_url, :liked, :source_url, :created_at, :updated_at
   has_many :artists, serializer: ArtistSummarizedSerializer
+  def image_url
+    if object.images.present?
+      return object.images[0]['url']
+    end
+    return "https://i.scdn.co/image/ab67616d0000b27304210aa081c36ce07355679c"
+  end
   def liked
     return false if scope.nil? || scope[:current_user].nil?
 
