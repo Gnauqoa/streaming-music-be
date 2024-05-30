b08a8f1ee67d9f82222cf29e84764253699dc185
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-05-30 10:08:05 +0700

    Add avatar_url to artist.

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :images, :genres, :birth, :followers_count, :description, :created_at, :updated_at
+  attributes :id, :name, :avatar_url, :genres, :birth, :followers_count, :description, :created_at, :updated_at
 end
